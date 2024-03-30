84c0c72a82ac8193b42408364da1b666ff783e6d
rainbow <64878693+shu-admin@users.noreply.github.com>
2024-03-30 18:38:52 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
             >
               <i class="el-icon-upload"></i>
               <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-              <div slot="tip" class="el-upload__tip">只能上传mp3/mp4文件，且不超过10mb,最多同时支持上传3个文件</div>
+              <div slot="tip" class="el-upload__tip">只能上传mp3/mp4文件，且不超过10mb,最多同时支持上传3个文件。https://cryptic-stream-41825-70b422ac31aa.herokuapp.com/</div>
             </el-upload>
             <el-input
               v-model="oneBestText"
